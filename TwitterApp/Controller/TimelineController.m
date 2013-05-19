4abf2a31e23e494927e953b199e456388c41add0
Petr Pavlik <pavlipe7@gmail.com>
2013-05-19 11:44:29 +0200

    progress on timeline controller

--- TwitterApp/Controller/TimelineController.m
+++ TwitterApp/Controller/TimelineController.m
@@ -6,33 +6,105 @@
 //  Copyright (c) 2013 Petr Pavlik. All rights reserved.
 //
 
+#import <Accounts/Accounts.h>
+#import "AFTwitterClient.h"
 #import "TimelineController.h"
+#import "TweetCell.h"
+#import "TweetEntity.h"
 
-@interface TimelineController ()
+@interface TimelineController () <UITableViewDataSource, UITableViewDelegate>
+
+@property(nonatomic, strong) UITableView* tableView;
+@property(nonatomic, strong) NSArray* tweets;
 
 @end
 
 @implementation TimelineController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    self.view.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    self.tableView = [[UITableView alloc] init];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    self.tableView.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:self.tableView];
+    
+    NSMutableArray* superviewConstraints = [NSMutableArray new];
+    [superviewConstraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_tableView]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(_tableView)]];
+    [superviewConstraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_tableView]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(_tableView)]];
+    
+    [self.view addConstraints:superviewConstraints];
+    
+    [self.tableView registerClass:[TweetCell class] forCellReuseIdentifier:@"TweetCell"];
+    
+    [self requestData];
 }
 
-- (void)didReceiveMemoryWarning
+#pragma mark -
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    return self.tweets.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"TweetCell";
+    TweetCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 100;
+}
+
+#pragma mark -
+
+- (void)requestData {
+    
+    ACAccountStore *accountStore = [[ACAccountStore alloc] init];
+    ACAccountType *accountType = [accountStore accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
+    
+    [accountStore requestAccessToAccountsWithType:accountType options:nil completion:^(BOOL granted, NSError *error) {
+        
+        if (granted) {
+            
+            NSArray *accounts = [accountStore accountsWithAccountType:accountType];
+            
+            // Check if the users has setup at least one Twitter account
+            if (accounts.count > 0) {
+                
+                ACAccount *twitterAccount = [accounts objectAtIndex:0];
+                
+                NSLog(@"%@", twitterAccount);
+                
+                [AFTwitterClient sharedClient].account = twitterAccount;
+                
+                [TweetEntity requestHomeTimelineWithCompletionBlock:^(NSArray *tweets, NSError *error) {
+                    NSLog(@"%@", tweets);
+                    self.tweets = tweets;
+                    [self.tableView reloadData];
+                }];
+                
+            }
+        } else {
+            
+            NSLog(@"No access granted %@", error);
+        }
+    }];
 }
 
 @end
