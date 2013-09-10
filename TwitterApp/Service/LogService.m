6a0c4e3d964aed9672f68d805f012dbf9ea7cfc2
Petr Pavlik <pavlipe7@gmail.com>
2013-09-10 14:41:35 +0200

    remove Google Analytics, implement Localytics

--- TwitterApp/Service/LogService.m
+++ TwitterApp/Service/LogService.m
@@ -6,12 +6,10 @@
 //  Copyright (c) 2013 Petr Pavlik. All rights reserved.
 //
 
-#import "GAI.h"
+#import "LocalyticsSession.h"
 #import "LogService.h"
 
 @interface LogService ()
-
-@property(nonatomic, strong) id <GAITracker> tracker;
 
 @end
 
@@ -34,35 +32,40 @@
     
     if (self) {
         
-        [GAI sharedInstance].trackUncaughtExceptions = NO;
-        [GAI sharedInstance].dispatchInterval = 0;
+        [[LocalyticsSession shared] startSession:@"3048d2b4028b670f856d4fc-f57032a8-d91f-11e2-0f5b-004a77f8b47f"];
         
 #ifdef DEBUG
-        [GAI sharedInstance].debug = YES;
+        [[LocalyticsSession shared] setLoggingEnabled:YES];
 #endif
-        // Create tracker instance.
-        self.tracker = [[GAI sharedInstance] trackerWithTrackingId:@"UA-43284408-1"];
         
-        [self.tracker sendEventWithCategory:@"Tweetilus for iPhone" withAction:@"app launched" withLabel:Nil withValue:Nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(goingBackgroundNotification:) name:UIApplicationDidEnterBackgroundNotification object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(goingBackgroundNotification:) name:UIApplicationWillTerminateNotification object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(goingForegroundNotification:) name:UIApplicationWillEnterForegroundNotification object:nil];
     }
     
     return self;
+}
+
+- (void)dealloc {
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 - (void)logError:(NSError*)error {
     
     NSParameterAssert(error);
     
-    NSLog(@"%@", error);
-    //[Flurry logError:@"App Error" message:Nil error:error];
-    [self.tracker sendException:NO withNSError:error];
+    NSLog(@"error: %@", error);
+    [[LocalyticsSession shared] tagEvent:@"error" attributes:@{@"error": error}];
 }
 
 - (void)logEvent:(NSString *)event userInfo:(NSDictionary *)userInfo {
     
     NSParameterAssert(event.length);
     
-    [self.tracker sendEventWithCategory:@"Tweetilus for iPhone" withAction:event withLabel:userInfo.description withValue:Nil];
+    NSLog(@"event: %@ userInfo: %@", event, userInfo);
+    
+    [[LocalyticsSession shared] tagEvent:event attributes:userInfo];
 }
 
 + (void)instatiate {
@@ -71,8 +74,22 @@
 
 - (void)setUserId:(NSString*)userId {
     
-    [self.tracker set:@"userId" value:userId];
-    //[Flurry setUserID:userId];
+    [[LocalyticsSession shared] setCustomerId:userId];
+    [[LocalyticsSession shared] setCustomerName:userId];
+}
+
+#pragma mark -
+
+- (void)goingBackgroundNotification:(NSNotification*)notification {
+    
+    [[LocalyticsSession shared] close];
+    [[LocalyticsSession shared] upload];
+}
+
+- (void)goingForegroundNotification:(NSNotification*)notificaiton {
+    
+    [[LocalyticsSession shared] resume];
+    [[LocalyticsSession shared] upload];
 }
 
 @end
